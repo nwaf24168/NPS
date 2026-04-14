217e21660b00a62ccfa105f30a0a3ebbdbe7dd5d
Nawaf Al-Shehri <nn121240@gmail.com>
2026-04-14 19:27:39 -0400

    إصلاح مسارات النشر وإضافة ملف النشر التلقائي لـ GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/NPS">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/dashboard" element={<Dashboard />} />
